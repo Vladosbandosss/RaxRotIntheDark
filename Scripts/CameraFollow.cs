0d3b65f7687915e23c56308b0e771c4751f23f30
Vladosbandosss <dasistperfektos@gmail.com>
2021-11-10 21:29:02 +0300

    RP
    RP

---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+  
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        Vector3 temp = transform.position;
+        temp.x = player.position.x;
+        temp.z = -10f;
+        transform.position = temp;
+    }
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject wave;
+
+    public float moveSpeed = 3f;
+    public float jumpForce = 500f;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    PlayerAnimation playerAnim;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+
+        playerAnim = GetComponent<PlayerAnimation>();
+    }
+    
+    void Update()
+    {
+        PlayerMove();
+        PlayerJump();
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (wave.activeInHierarchy)
+            {
+                return;
+            }
+            else
+            {
+                StartCoroutine(nameof(LightTheWay));
+            }
+        }
+    }
+
+    IEnumerator LightTheWay()
+    {
+        wave.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        wave.SetActive(false);
+    }
+
+    private void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (rb.velocity.y != 0f)
+            {
+                return;
+            }
+
+            rb.AddForce(Vector2.up* Time.deltaTime*jumpForce, ForceMode2D.Impulse);
+
+            playerAnim.Jump(true);
+
+
+        }
+    }
+
+    private void PlayerMove()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if (h > 0) {
+            Vector2 temp = transform.position;
+            temp.x += moveSpeed * Time.deltaTime;
+            transform.position = temp;
+
+            sr.flipX = false;
+
+            playerAnim.Walk(true);
+        }
+        else if (h < 0)
+        {
+            Vector2 temp = transform.position;
+            temp.x -= moveSpeed * Time.deltaTime;
+            transform.position = temp;
+
+            sr.flipX = true;
+
+            playerAnim.Walk(true);
+        }
+        else
+        {
+            playerAnim.Walk(false);
+        }
+
+        if (Mathf.Abs(rb.velocity.y) <= 0.01f)
+        {
+            playerAnim.Jump(false);
+        }
+    }
+}
